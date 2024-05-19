d1aa1685f01e5a7a8c372116d415c04ec45e5bb2
rivka-yuliia <juliacreadora@gmail.com>
2024-05-19 15:33:11 +0300

    subclasses in tests

--- tests/test_custom_validators.py
+++ tests/test_custom_validators.py
@@ -7,22 +7,14 @@ from unittest import TestCase
 from app.validators import IntegerField, CharField
 
 
-class Point:
-    x = IntegerField(min_value=1, max_value=100)
-
-    def __init__(self, x):
-        self.x = x
-
-
-class Person:
-    name = CharField(min_value=1, max_value=10)
-
-    def __init__(self, name):
-        self.name = name
-
-
 class TestIntegerField(TestCase):
     """Tests for IntegerField objects."""
+
+    class Point:
+        x = IntegerField(min_value=1, max_value=100)
+
+        def __init__(self, x):
+            self.x = x
 
     def setUp(self):
         self.prop_name = 'x'
@@ -33,21 +25,21 @@ class TestIntegerField(TestCase):
     def test_set_name(self):
         """Test set property name."""
 
-        p = Point(self.prop_value)
+        p = self.Point(self.prop_value)
 
         self.assertIn(self.prop_name, p.__dict__)
 
     def test_set_value_success(self):
         """Test set property value successfully."""
 
-        p = Point(self.prop_value)
+        p = self.Point(self.prop_value)
 
         self.assertEqual(p.x, self.prop_value)
 
     def test_get_property_value(self):
         """Test getting property value."""
 
-        p = Point(self.prop_value)
+        p = self.Point(self.prop_value)
         value = p.x
 
         self.assertEqual(value, self.prop_value)
@@ -55,30 +47,36 @@ class TestIntegerField(TestCase):
     def test_min_max_values_success(self):
         """Test set minimum and maximum values successfully."""
 
-        self.assertEqual(Point.x.min_value, self.prop_min)
-        self.assertEqual(Point.x.max_value, self.prop_max)
+        self.assertEqual(self.Point.x.min_value, self.prop_min)
+        self.assertEqual(self.Point.x.max_value, self.prop_max)
 
     def test_non_integer_value_error(self):
         """Test set non integer value raises TypeError."""
 
         with self.assertRaises(TypeError):
-            Point(3.5)
+            self.Point(3.5)
 
     def test_wrong_min_value_error(self):
         """Test set value less than minimum raises ValueError."""
 
         with self.assertRaises(ValueError):
-            Point(-1)
+            self.Point(-1)
 
     def test_wrong_max_value_error(self):
         """Test set value greater than maximum raises ValueError."""
 
         with self.assertRaises(ValueError):
-            Point(200)
+            self.Point(200)
 
 
 class TestCharField(TestCase):
     """Tests for CharField objects."""
+
+    class Person:
+        name = CharField(min_value=1, max_value=10)
+
+        def __init__(self, name):
+            self.name = name
 
     def setUp(self):
         self.prop_name = 'name'
@@ -89,21 +87,21 @@ class TestCharField(TestCase):
     def test_set_name(self):
         """Test set property name."""
 
-        p = Person(self.prop_value)
+        p = self.Person(self.prop_value)
 
         self.assertIn(self.prop_name, p.__dict__)
 
     def test_set_value_success(self):
         """Test set property value successfully."""
 
-        p = Person(self.prop_value)
+        p = self.Person(self.prop_value)
 
         self.assertEqual(p.name, self.prop_value)
 
     def test_get_property_value(self):
         """Test getting property value."""
 
-        p = Person(self.prop_value)
+        p = self.Person(self.prop_value)
         value = p.name
 
         self.assertEqual(value, self.prop_value)
@@ -111,23 +109,23 @@ class TestCharField(TestCase):
     def test_min_max_values_success(self):
         """Test set minimum and maximum values successfully."""
 
-        self.assertEqual(Person.name.min_value, self.prop_min)
-        self.assertEqual(Person.name.max_value, self.prop_max)
+        self.assertEqual(self.Person.name.min_value, self.prop_min)
+        self.assertEqual(self.Person.name.max_value, self.prop_max)
 
     def test_non_string_value_error(self):
         """Test set non string value raises TypeError."""
 
         with self.assertRaises(TypeError):
-            Person(10)
+            self.Person(10)
 
     def test_empty_string_error(self):
         """Test set empty string raises ValueError."""
 
         with self.assertRaises(ValueError):
-            Person('')
+            self.Person('')
 
     def test_wrong_max_value_error(self):
         """Test set value greater than maximum length raises ValueError."""
 
         with self.assertRaises(ValueError):
-            Person('Eadfgvsdvjkl')
+            self.Person('Eadfgvsdvjkl')
